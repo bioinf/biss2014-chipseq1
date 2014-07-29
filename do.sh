f2da82a08dcd0d5206fa8894d7e835bbef9e8e72
Alexey Sergushichev <alsergbox@gmail.com>
2014-07-30 01:48:20 +0400

    Filtering sam and running macs2

--- do.sh
+++ do.sh
@@ -30,10 +30,24 @@ fastqc work/$BASE.ltrimmed.fastq
     mkdir hg19;
     cd hg19; 
     tar -xzf ../chromFa.tar.gz
-    bowtie2-build `ls *.fa | grep -P "chr(\d{1,2}|X|Y).fa" | tr "\n" "," | sed "s/,$//"` hg19
+    cat `ls *.fa | grep -P "chr(\d{1,2}|X|Y).fa"` > hg19.fasta
+    bowtie2-build hg19.fasta hg19
 )
 
 # mapping
 
 bowtie2 -p 4 -x data/hg19/hg19 -U $INFILE -S work/$BASE.sam
 #bowtie2 -p 4 -x data/hg19/hg19 -U work/$BASE.qtrimmed.fastq -S wor/$BASE.ltrimmed.sam
+
+samtools view -bS work/$BASE.sam -o work/$BASE.bam
+samtools sort work/$BASE.bam work/$BASE.sorted
+samtools index work/$BASE.sorted.bam
+
+# :ToDo: what -F 4 means?
+samtools view -b -F 4 -q 1 work/$BASE.sorted.bam -o work/$BASE.filtered.bam
+samtools index work/$BASE.filtered.bam
+
+macs2 -t work/$BASE.filtered.bam -g 1e9 --name=work/IRF1 --format=BAM --tsize=36 --mfold=10,30
+
+
+
