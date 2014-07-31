b08429795715e787b313a91b69e157eaf2e720b2
Alexey Sergushichev <alsergbox@gmail.com>
2014-07-31 09:35:21 +0400

    new options for trimming, mapping and macs

--- do.sh
+++ do.sh
@@ -10,13 +10,13 @@ mkdir work
 fastqc $INFILE
 
 # trimming by quality
-fastq_quality_trimmer -v -t 28 -l 36 -i $INFILE -o work/$BASE.qtrimmed.fastq
+fastq_quality_trimmer -Q 64 -v -t 28 -l 30 -i $INFILE -o work/$BASE.qtrimmed.fastq |& ts | tee work/$BASE.qtrimmed.log
 
 # quality control
 fastqc work/$BASE.qtrimmed.fastq
 
 # trimming by length
-fastx_trimmer -v -f 1 -l 75 -i $INFILE -o work/$BASE.ltrimmed.fastq
+#fastx_trimmer -v -f 1 -l 75 -i $INFILE -o work/$BASE.ltrimmed.fastq
 
 # quality control
 fastqc work/$BASE.ltrimmed.fastq
@@ -36,18 +36,38 @@ fastqc work/$BASE.ltrimmed.fastq
 
 # mapping
 
-bowtie2 -p 4 -x data/hg19/hg19 -U $INFILE -S work/$BASE.sam
-#bowtie2 -p 4 -x data/hg19/hg19 -U work/$BASE.qtrimmed.fastq -S wor/$BASE.ltrimmed.sam
+bowtie2 -p 4 -x data/hg19/hg19 -U $INFILE -S work/$BASE.sam |& ts | tee work/$BASE.sam.log
+bowtie2 -p 4 -x data/hg19/hg19 -U work/$BASE.qtrimmed.fastq -S work/$BASE.qtrimmed.sam |& ts | tee work/$BASE.qtrimmed.sam.log
+bowtie2 -p 4 -t -k 1 -x data/hg19/hg19 -U work/$BASE.qtrimmed.fastq -S work/$BASE.qtrimmed.nomm.sam |& ts | tee work/$BASE.qtrimmed.nomm.sam.log
+bowtie2 -p 4 -t -k 1 -x data/hg19/hg19 -U work/$BASE.qtrimmed.fastq -S work/$BASE.qtrimmed.nomm.sam |& ts | tee work/$BASE.qtrimmed.nomm.sam.log
 
-samtools view -bS work/$BASE.sam -o work/$BASE.bam
-samtools sort work/$BASE.bam work/$BASE.sorted
-samtools index work/$BASE.sorted.bam
+function sam_sort {
+    SAM=$1
+    BAM=${1/sam/bam}
+    SORTED=${1/sam/sorted}
+    samtools view -bS $1 -o $BAM
+    samtools sort $BAM $SORTED
+    samtools index $SORTED.bam
+}
 
-# :ToDo: what -F 4 means?
-samtools view -b -F 4 -q 1 work/$BASE.sorted.bam -o work/$BASE.filtered.bam
-samtools index work/$BASE.filtered.bam
+sam_sort work/$BASE.sam
+sam_sort work/$BASE.qtrimmed.sam
 
-macs2 -t work/$BASE.filtered.bam -g 1e9 --name=work/IRF1 --format=BAM --tsize=36 --mfold=10,30
+function sam_filter {
+    BAM=$1
+    OPTIONS="$2"
+    FILTERED=${BAM/bam/filtered.bam}
+
+    samtools view -b $OPTIONS $BAM -o $FILTERED
+    samtools index $FILTERED
+}
+
+sam_filter work/$BASE.sorted.bam "-F 4 -q 1"
+sam_filter work/$BASE.qtrimmed.sorted.bam "-q 30"
+
+#macs2 -t work/$BASE.filtered.bam -g 1e9 --name=work/IRF1 --format=BAM --tsize=36 --mfold=10,30
+macs2 -t work/$BASE.filtered.bam -g hs --name=work/IRF1.2 --format=BAM --tsize=36 --mfold=10,30 |& ts | tee work/$BASE.IRF1.2.log
+macs2 -t work/$BASE.qtrimmed.sorted.filtered.bam -g hs --name=work/IRF1.3 --format=BAM --tsize=36 --mfold=10,30 |& ts | tee work/$BASE.IRF1.3.log
 
 
 
